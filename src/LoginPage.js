b6b6245c32c96eb790309b5e92c045b548a63279
praka <prakashswami150569@gmail.com>
2026-03-28 04:16:47 +0530

    4.0

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect, useRef } from "react";
 import { supabase } from "./supabaseClient";
